7b4c7e6ad47bb91d120f9c94db63367d6f4f894e
Noir <haydnady@gmail.com>
2021-03-14 14:41:38 -0400

    file search dialog code added

--- flacToMpThree.py
+++ flacToMpThree.py
@@ -1,4 +1,23 @@
+from tkinter.filedialog import askopenfilenames
 from pydub import AudioSegment
+import tkinter as tk
 
-sound = AudioSegment.from_file("/input/file")
-sound.export("/output/file", format="mp3", bitrate="320k")
+
+def getFile():
+    """
+        Uses tkinter to open a file dialog box and allows user to selec file(s).
+        @return File paths tuple of seslected items through tkinter GUI 
+    """
+    root = tk.Tk()
+    files = askopenfilenames(parent=root, title="Select file(s)")
+    root.quit()
+    root.destroy()
+
+    return(files)
+
+
+# file = getFile()[0]
+# print(file)
+
+sound = AudioSegment.from_file("............", format="flac")
+# sound.export("........mp3", format="mp3", bitrate="320k")
