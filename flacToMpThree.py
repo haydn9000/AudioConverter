ddad18a6712d6dc210d56cf1f10c1c9812109f88
Noir <haydnady@gmail.com>
2021-03-21 12:50:40 -0400

    Updating code to have more functions and be more modular

--- flacToMpThree.py
+++ flacToMpThree.py
@@ -1,3 +1,7 @@
+"""
+    This module is for converting FLAC to MP3 with all the metadata available
+"""
+
 from tkinter.filedialog import askopenfilenames
 from pydub.utils import mediainfo
 from pydub import AudioSegment
@@ -9,8 +13,8 @@ import os
 
 def getFile():
     """
-        Uses tkinter to open a file dialog box and allows user to selec file(s).
-        @return File paths tuple of seslected items through tkinter GUI 
+    Uses tkinter to open a file dialog box and allows user to selec file(s).
+    @return File paths tuple of seslected items through tkinter GUI 
     """
     root = tk.Tk()
     files = askopenfilenames(parent=root, title="Select file(s)")
@@ -20,21 +24,32 @@ def getFile():
     return(files)
 
 
-# Convert to to selected format
-file = getFile()[0]
-outExtension = ".mp3"
-fileName = os.path.splitext(file)[0] + outExtension
-print(fileName)
+def convertFlacToMpthree(file, outExtension=".mp3"):
+    fileName = os.path.splitext(file)[0] + outExtension
+    print(fileName)
 
-sound = AudioSegment.from_file(file)
+    sound = AudioSegment.from_file(file)
 
-sound.export(fileName,
-             format="mp3",
-             bitrate="320k",
-             tags=mediainfo(file)["TAG"])
+    sound.export(fileName,
+                format="mp3",
+                bitrate="320k",
+                tags=mediainfo(file)["TAG"])
+
+    # Add album cover
+    audiofile = eyed3.load(fileName)
+    if (audiofile.tag == None):
+        audiofile.initTag()
+
+    audiofile.tag.images.set(3, open('cover.jpg','rb').read(), 'image/jpeg')
+
+    """
+    You have to set the ID3 version to "V2.3", otherwise the photo won't show up for the file icon.
+    https://stackoverflow.com/questions/38510694/how-to-add-album-art-to-mp3-file-using-python-3#39316853
+    """
+    audiofile.tag.save(version=eyed3.id3.ID3_V2_3)
 
 
-def getCoverArt(file):
+def getFlacAudioCoverArt(file):
     """ Get sound front cover """
     var = FLAC(file)
     pics = var.pictures
@@ -47,11 +62,6 @@ def getCoverArt(file):
                 f.write(p.data)
 
 
-# Add album cover
-audiofile = eyed3.load(fileName)
-if (audiofile.tag == None):
-    audiofile.initTag()
 
-audiofile.tag.images.set(3, open('cover.jpg','rb').read(), 'image/jpeg')
-
-audiofile.tag.save(version=eyed3.id3.ID3_V2_3)
+if __name__ == "__main__":
+    pass
